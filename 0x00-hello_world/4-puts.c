50109285e89cce00ccdf4c0bec64f761fe3ec107
Toby16 <bolajibaasit14@gmail.com>
2022-10-13 04:33:39 -0700

    added 4-puts.c FILE

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-/*
- * main() - Entry
- */
+/**
+ * main - Entry
+ * Return: Always 0
+*/
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle");
 
-return (0);
+	return (0);
 }
