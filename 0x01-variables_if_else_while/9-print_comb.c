5e4e6619a5f6f6e02b882a2ade6abcee54fee077
Toby16 <bolajibaasit14@gmail.com>
2022-10-14 16:11:23 -0700

    added 9-print_comb.c FILE

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int num_value = 0;
+
+	for (; num_value <= 9; num_value++)
+	{
+		putchar((num_value % 10) + '0')
+		if (num == 9)
+		{
+			continue;
+		}
+		putchar(', ');
+	}
+
+	return (0);
+}
