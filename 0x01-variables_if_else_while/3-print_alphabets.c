9eb6eb0f1f3e65487ffb187e1246a832a4516496
Toby16 <bolajibaasit14@gmail.com>
2022-10-14 14:38:27 -0700

    added 3-print_alphabets.c FILE

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,11 +8,12 @@
 int main(void)
 {
 	char ch_A, ch_B;
+
 	for (ch_A = 'a'; ch_A <= 'z'; ch_A++)
 	{
 		putchar(ch_A);
 	}
-	for( ch_B = 'A'; ch_B <= 'Z'; ch_B++)
+	for (ch_B = 'A'; ch_B <= 'Z'; ch_B++)
 	{
 		putchar(ch_B);
 	}
