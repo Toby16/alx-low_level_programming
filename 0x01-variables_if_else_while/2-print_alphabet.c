20651160ebc5a8e1d051cd04a11304a78df18d1c
Toby16 <bolajibaasit14@gmail.com>
2022-10-14 14:21:35 -0700

    added 2-print_alphabet.c FILE

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+
+		putchar(ch);
+
+	}
+	putchar('\n');
+
+	return (0);
+}
