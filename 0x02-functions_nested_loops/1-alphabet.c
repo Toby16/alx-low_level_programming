a84dc4c9ac82886b08f4161bcb5cdf33c3e7a765
Toby16 <bolajibaasit14@gmail.com>
2022-10-18 11:51:24 -0700

    .

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function to be included in the main()
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+	letter = 'a';
+
+	for (; letter <= 'z'; letter+=)
+	{
+		_putchar(letter);
+	}
+	putchar('\n');
+}
