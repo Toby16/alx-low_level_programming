8847ddb8bae978a914be22ac561ad8a3c075fde9
Toby16 <bolajibaasit14@gmail.com>
2022-10-18 13:58:16 -0700

    .

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,6 +3,7 @@
 /**
  * _islower - entry to main function
  * @c: parameter for _islower
+ * test_islower - function1
  * Return: 1 if character is lowercase, 0 otherwise
  */
 
@@ -16,4 +17,19 @@ int _islower(int c)
 	{
 		return (0);
 	}
+}
+
+/**
+ * test_islower - function1
+ * @n: parameter1
+ * Return: Always 0
+ */
+void test_islower(int n)
+{
+	int r;
+
+	r = _islower(n);
+
+	putchar(r + '0');
+	putchar('\n');
 }
