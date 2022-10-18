5ec315060aa04b49020f6a6960fbfd96b739a942
Toby16 <bolajibaasit14@gmail.com>
2022-10-18 15:23:50 -0700

    .

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,18 +10,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
+		_putchar('+');
 		return (1);
 	}
 	else if (n < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		return (-1);
 	}
 	else
 	{
-		putchar('0');
+		_putchar('0');
 		return (0);
 	}
-	putchar('\n');
 }
