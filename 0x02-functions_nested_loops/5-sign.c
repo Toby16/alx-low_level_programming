fec00ba27a5f59484790cdd739e33950d2df0f2b
Toby16 <bolajibaasit14@gmail.com>
2022-10-18 15:11:14 -0700

    .

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - Entry
+ * Return: 1, else 0 else -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		putchar('-');
+		return (-1);
+	}
+	else
+	{
+		putchar('0');
+		return (0);
+	}
+}
