8cc621bfb31b2130fc6a521619383e82b6d4392b
Toby16 <bolajibaasit14@gmail.com>
2022-10-18 19:14:34 -0700

    .

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Entry
+ * @n: parameter
+ * Return: 0
+ */
+
+void print_to_98(int n)
+{
+	int val = 98;
+
+	if (n <= val)
+	{
+		while (n <= val)
+		{
+			printf("%d", n);
+
+
+			if (n == val)
+			{
+				break;
+			}
+			putchar(',');
+			putchar(' ');
+			n++;
+		}
+	}
+	else if (n >= val)
+	{
+		while (n >= val)
+		{
+			printf("%d", n);
+
+			if (n == val)
+			{
+				break;
+			}
+			putchar(',');
+			putchar(' ');
+			n--;
+		}
+	}
+}
