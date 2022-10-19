a75a22a3da8b3534e64265c80c14312ae17c049b
Toby16 <bolajibaasit14@gmail.com>
2022-10-19 06:39:59 -0700

    .

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int r;
+
+	positive_or_negative(r);
+
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+/**
+ * File: main.h
+ * Description:  Header file containing declarations for functions used in the 0x03-debugging directory
+ */
+
+void positive_or_negative(int i);
+
+#endif
