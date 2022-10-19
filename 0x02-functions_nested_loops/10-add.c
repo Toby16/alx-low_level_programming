74c5f61e840c3d845a7d29b55451a1f555e4155a
Toby16 <bolajibaasit14@gmail.com>
2022-10-18 18:27:30 -0700

    .

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Entry
+ * @x: value of x
+ * @y: value of y
+ * Return: sum or x and y
+ */
+
+int add(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+	return (sum);
+}
