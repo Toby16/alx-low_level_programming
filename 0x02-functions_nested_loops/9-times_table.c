e18e4c1a15f8a3ce485d112b079d180235d4f774
Toby16 <bolajibaasit14@gmail.com>
2022-10-18 17:53:48 -0700

    .

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,7 +12,7 @@ void times_table(void)
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
-		for (mult = 1; mult <= 9; mult++)
+		for (multiply = 1; multiply <= 9; multiply++)
 		{
 			_putchar(',');
 			_putchar(' ');
@@ -25,9 +25,9 @@ void times_table(void)
 			}
 			else
 			{
-				_putchar((prod / 10) + '0');
+				_putchar((value / 10) + '0');
 			}
-			_putchar((prod % 10) + '0');
+			_putchar((value % 10) + '0');
 		}
 		putchar('\n');
 	}
