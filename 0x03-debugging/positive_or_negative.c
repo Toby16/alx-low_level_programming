b0c97e199a081416969bebb3972c32a01b351458
Toby16 <bolajibaasit14@gmail.com>
2022-10-19 07:16:57 -0700

    .

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * positive_or_negative - Entry
+ * @i: integer parameter
+ * Return: Always 0
+ */
+
+void positive_or_negative(int i)
+{
+	if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+	else if (i > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else
+	{
+		printf("%d is zero\n", n);
+	}
+	return (0);
+}
