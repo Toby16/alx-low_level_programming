3ab338fe1b73e28d4f4e4cd0233709a5b9846e24
Toby16 <bolajibaasit14@gmail.com>
2022-10-20 13:31:13 -0700

    .

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <main.h>
+
+/**
+ * print_numbers - function
+ * Return: value
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	for (; i <= 9; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
