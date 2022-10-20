3568d733c3e103976d55f6f08638da66bf9a335a
Toby16 <bolajibaasit14@gmail.com>
2022-10-20 16:44:33 -0700

    .

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_square - function
+ * @size: int parameter
+ * return: value
+ */
+
+void print_square(int size)
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		int j;
+
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
