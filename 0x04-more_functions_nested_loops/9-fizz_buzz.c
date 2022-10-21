81a1d1c127b16401c7ed4791591e1fd697128342
Toby16 <bolajibaasit14@gmail.com>
2022-10-20 17:45:51 -0700

    .

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry
+ * Return: 0, if no error
+ */
+
+int main(void)
+{
+	int i = 1;
+
+	for (; i <= 100; i++)
+	{
+		if (((i % 3) == 0) && ((i % 5) != 0))
+		{
+			printf(" Fuzz");
+		}
+		else if (((i % 5) == 0) && ((i % 3) != 0))
+		{
+			printf(" Buzz");
+		}
+		else if ((i % 3 == 0) && (i % 5) == 0)
+		{
+			printf(" FizzBuzz");
+		}
+		else if (i == 1)
+		{
+			printf("%d", i);
+		}
+		else
+		{
+			printf(" %d", i);
+		}
+	}
+	_putchar('\n');
+
+	return (0);
+}
