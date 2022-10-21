b27fa9c4830101cbb6b61b09e1ff820368616f61
Toby16 <bolajibaasit14@gmail.com>
2022-10-20 18:10:02 -0700

    .

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -22,16 +22,19 @@ int main(void)
 		{
 			printf(" FizzBuzz");
 		}
-		else if (i == 1)
-		{
-			printf("%d", i);
-		}
 		else
 		{
-			printf(" %d", i);
+			if (i == 1)
+			{
+				printf("%d", i);
+			}
+			else
+			{
+				printf(" %d", i);
+			}
 		}
-	}
-	printf("\n");
+		printf("\n");
 
-	return (0);
+		return (0);
+	}
 }
