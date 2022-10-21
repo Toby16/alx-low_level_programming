52e99d0bf5611511ce4fd6083735b5ed57448720
Toby16 <bolajibaasit14@gmail.com>
2022-10-20 18:26:28 -0700

    .

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,35 +8,31 @@
 
 int main(void)
 {
-	int i = 1;
+	int i;
 
-	for (; i <= 100; ++i)
+	for (i = 0; i <= 100; i++)
 	{
-		if (((i % 3) == 0) && ((i % 5) != 0))
+		if ((i % 15) == 0)
 		{
-			printf(" Fizz");
+			printf("FizzBuzz");
 		}
-		else if (((i % 5) == 0) && ((i % 3) != 0))
+		else if ((i % 3) == 0)
 		{
-			printf(" Buzz");
+			printf("Fizz");
 		}
-		else if ((i % 3 == 0) && (i % 5) == 0)
+		else if ((i % 5) == 0)
 		{
-			printf(" FizzBuzz");
+			printf("Buzz");
 		}
 		else
 		{
-			if (i == 1)
-			{
-				printf("%d", i);
-			}
-			else
-			{
-				printf(" %d", i);
-			}
+			printf("%d", i);
 		}
-		printf("\n");
 
-		return (0);
+		if (i < 100)
+		{
+			printf(" ");
+		}
+		i++;
 	}
 }
