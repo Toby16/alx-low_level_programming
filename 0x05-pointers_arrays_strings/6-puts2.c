0a6f9a5f71ccf210c909891b45f7a17241159070
Toby16 <bolajibaasit14@gmail.com>
2022-10-24 12:53:44 -0700

    .

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - function
+ * @str: parameter
+ * Return: Value
+ */
+
+void puts2(char *str)
+{
+	int x = 0;
+	int y = 0;
+
+	while (str[x++])
+	{
+		y++;
+	}
+
+	for (x = 0; x < y; x++)
+	{
+		_putchar(str[x]);
+	}
+	_putchar('\n');
+}
