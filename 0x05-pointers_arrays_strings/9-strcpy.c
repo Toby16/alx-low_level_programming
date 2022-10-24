7fffe68c440f9f4d487ee5077cad9a22724481e0
Toby16 <bolajibaasit14@gmail.com>
2022-10-24 13:38:19 -0700

    .

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy - function
+ * @dest: parameter
+ * @src: parameter
+ * Return: Value
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int x = 0;
+
+	while (src[x])
+	{
+		dest[index] = src[index];
+
+		index++;
+	}
+
+	return (dest);
+}
