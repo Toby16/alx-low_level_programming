80973b5d33cc5caa879bf30414c87c50d9df5433
Toby16 <bolajibaasit14@gmail.com>
2022-10-24 13:17:51 -0700

    .

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts_half - function
+ * @str: parameter
+ * Return: Value
+ */
+
+void puts_half(char *str)
+{
+	int x, y;
+
+	x = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	y = (x + 1) / 2;
+
+	for (x = y; str[x]; x++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
