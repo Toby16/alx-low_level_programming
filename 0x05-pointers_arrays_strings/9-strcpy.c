0a1c988d077e2b0a4d821138ee6b7b6e1e77f41d
Toby16 <bolajibaasit14@gmail.com>
2022-10-24 14:59:06 -0700

    .

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,14 +11,12 @@ char *_strcpy(char *dest, char *src)
 {
 	int x = 0;
 
-	while (x >= 0)
+	while (src[x] != '\0')
 	{
-		*(dest + x) = *(src + x);
-		if (*(src + x) == '\0')
-		{
-			break;
-		}
+		dest[x] = src[x];
+		x++;
 	}
+	dest[x] = '\0';
 
 	return (dest);
 }
