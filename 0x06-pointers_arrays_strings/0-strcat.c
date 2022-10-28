991a882debdd385de8a91109357b1ff3a3c27057
Toby16 <bolajibaasit14@gmail.com>
2022-10-27 18:14:40 -0700

    .

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,18 +11,20 @@ char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
-	for (i = 0; *(dest + i) != '\0'; i++)
+	i = 0;
+	j = 0;
+
+	while (*(dest + i) != '\0')
 	{
-		if (i == 4)
-		{
-			*(dest + 5) = ' ';
-		}
-	}
-	for (j = 0; *(dest + j) != '\0'; j++)
-	{
-		*(dest + i) = *(src + j);
+		i += 1;
 	}
 
+	while (*(src + j) != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++
+	}
 	*(dest + i) = '\0';
 
 	return (*(&dest));
