4051489e06f92eda9e09c67f81a72427542ca2f1
Toby16 <bolajibaasit14@gmail.com>
2022-10-27 19:28:48 -0700

    .

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcmp - Function
+ * @s1: Parameter;
+ * @s2: Parameter
+ * Return: *s1 - *s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && &*s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+
+	return (*s1 - *s2);
+}
