f6c569347cce87f7e157d67653dd6cdde7b57f01
Toby16 <bolajibaasit14@gmail.com>
2022-10-29 21:27:08 -0700

    .

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,11 +7,11 @@
 
 void print_alphabet(void)
 {
-	char letter;
+	int a;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+	for (a = 97; a <= 122; a++)
 	{
-		_putchar(letter);
+		putchar(a);
 	}
 	_putchar('\n');
 }
