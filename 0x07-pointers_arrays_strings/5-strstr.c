fd68718518a2fb1a89d933da46d1c9755c38cdb6
Toby16 <bolajibaasit14@gmail.com>
2022-10-31 19:38:00 -0700

    .

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * _strstr - Function
+ * @haystack: parameter
+ * @needle: parameter
+ * Return: haystack / 0
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *l = haystack;
+		char *p = needle;
+
+		while (*l == *p && *p != '\0')
+		{
+			l++;
+			p++;
+		}
+		if (*p == '\0')
+		{
+			return (haystack);
+		}
+	}
+	return (0);
+}
