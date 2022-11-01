0353d29c13c7a09a9807bf12785578fbd250630a
Toby16 <bolajibaasit14@gmail.com>
2022-10-31 19:55:55 -0700

    .

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
-/** print_chessboard - function
+/**
+ * print_chessboard - function
  * @a: parameter
+ * Return: value
  */
 
 void print_chessboard(char (*a)[8])
@@ -10,7 +12,7 @@ void print_chessboard(char (*a)[8])
 
 	for (i = 0; i < 8; i++)
 	{
-		for(j = 0; j < 8; j++)
+		for (j = 0; j < 8; j++)
 		{
 			_putchar(a[i][j]);
 		}
