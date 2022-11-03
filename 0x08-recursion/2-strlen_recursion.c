ee2f62e6c4981b9deae266119001f69ffea3d268
Toby16 <bolajibaasit14@gmail.com>
2022-11-02 17:31:41 -0700

    .

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Function
+ * @s: parameter
+ * Return: Value
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
