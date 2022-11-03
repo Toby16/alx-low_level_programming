36afda753675b195c5507eac7f0d992fd220b4bd
Toby16 <bolajibaasit14@gmail.com>
2022-11-02 19:57:34 -0700

    .

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * is_prime_number - Function
+ * @n: parameter
+ * Return: value
+ */
+
+int is_prime_number(int n)
+{
+	if (n == 0)
+	{
+		return (0);
+	}
+	if (n < 0)
+	{
+		return (0);
+	}
+	if (n == 1)
+	{
+		return (0);
+	}
+	else
+	{
+		return (is_prime(n, 2));
+	}
+}
+
+/**
+ * is_prime - Function
+ * @n: parameter
+ * @c: parameter
+ * Return: value
+ */
+
+int is_prime(unsigned int n, unsigned int c)
+{
+	if (n % c == 0)
+	{
+		if (n == c)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	else
+	{
+		return (0 + is_prime(n, c + 1));
+	}
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,5 +10,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int find_sqrt(int num, int root);
+int is_prime_number(int n);
+int is_divisible(int num, int div);
 
 #endif
