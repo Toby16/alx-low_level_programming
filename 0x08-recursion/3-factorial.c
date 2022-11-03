78513fa82fb01d4848a60f18914abb445ad96043
Toby16 <bolajibaasit14@gmail.com>
2022-11-02 17:55:58 -0700

    .

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * factorial - Function
+ * @n: parameter
+ * Return: Value
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
