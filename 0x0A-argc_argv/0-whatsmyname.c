945137cc0c6cb1776064aed604395be17cabde82
Toby16 <bolajibaasit14@gmail.com>
2022-11-04 15:51:24 -0700

    .

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Function
+ * @argc: argument
+ * @argv: argument
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
