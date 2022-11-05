395fb8250aeb20d8ef33b8e700182289064c84ed
Toby16 <bolajibaasit14@gmail.com>
2022-11-04 17:10:47 -0700

    .

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Function
+ * @argc: argument
+ * @argv: argument
+ * Return: 0;
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
