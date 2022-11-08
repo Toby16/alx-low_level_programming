b79972276ed46c41bf9b69a88716ef242f4f1e85
Toby16 <bolajibaasit14@gmail.com>
2022-11-08 09:48:27 -0800

    .

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * create_array - Function
+ * @size: Parameter
+ * @c: Parameter
+ * Return: ptr
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	int i;
+
+	ptr = malloc(sizeof(*ptr) * size);
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		if (ptr == NULL)
+		{
+			return (NULL);
+		}
+		else
+		{
+			for(i = 0; i < size; i++)
+			{
+				ptr[i] = c;
+			}
+		}
+	}
+	return (ptr);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+
+#endif
