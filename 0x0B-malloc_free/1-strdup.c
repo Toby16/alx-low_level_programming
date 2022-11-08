879f4c59a067dc20ea55184915f2d02167cfc7b1
Toby16 <bolajibaasit14@gmail.com>
2022-11-08 10:07:53 -0800

    .

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strdup - Functions
+ * @str:  parameter
+ * Return: ptr
+ */
+
+char *_strdup(char *str)
+{
+	char *ptr;
+	int i;
+
+	ptr = mallos(sizeof(*str));
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	
+	if (ptr == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; str[i]; i++)
+		{
+			ptr[i] = str[i];
+		}
+	}
+	return (ptr);
+}
