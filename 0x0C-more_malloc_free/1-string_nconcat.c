7030e3d3beaee2f3ca4c3956c880bc5332fc442e
Toby16 <bolajibaasit14@gmail.com>
2022-11-10 15:20:52 -0800

    .'

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -33,7 +33,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	j = 0;
 
-	for  (i = 0; s1[i] != '\0'; i++)
+	for  (i = 0; s1[i]; i++)
 	{
 		ptr[j++] = s1[i];
 	}
@@ -42,6 +42,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		ptr[j++] = s2[i];
 	}
-	ptr[i] = '\0';
+	ptr[j] = '\0';
 	return (ptr);
 }
