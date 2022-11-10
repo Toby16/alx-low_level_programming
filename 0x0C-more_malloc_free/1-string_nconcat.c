3e67dcb5cfed362ff37cc823e7f31ec9ed6adffc
Toby16 <bolajibaasit14@gmail.com>
2022-11-10 14:52:46 -0800

    .

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,7 +21,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	j = n;
 
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; s1[i]; i++)
 		j += 1;
 
 	ptr = malloc(sizeof(*ptr) * (j * 1));
