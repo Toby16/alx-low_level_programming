b3c551dc3a989bd5fcd65c2c4f821f7d2800d295
Toby16 <bolajibaasit14@gmail.com>
2022-11-14 15:46:16 -0800

    .

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - Function
+ * @d: parameter
+ * @name: parameter
+ * @age: parameter
+ * @owner: parameter
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct dog - structure
@@ -14,5 +16,8 @@ struct dog
 	float age;
 	char *owner;
 };
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
