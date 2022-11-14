379753e7ff4f764557e42954895e064bc83bc0b8
Toby16 <bolajibaasit14@gmail.com>
2022-11-14 15:25:32 -0800

    .

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - structure
+ * @name: element
+ * @age: element
+ * @owner: element
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
