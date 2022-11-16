2eeda46f15de2a68e35291fb0cd36bb8ce90fe60
Toby16 <bolajibaasit14@gmail.com>
2022-11-16 18:38:00 -0500

    .

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,36 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - Function
+ * @array: parameter
+ * @size: parameter
+ * @cmp: parameter
+ * Return: Value
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && cmp)
+	{
+		if (size <= 0)
+		{
+			return (-1);
+		}
+		else
+		{
+			for (i = 0; i < size; i++)
+			{
+				if (cmp(array[i]))
+				{
+					return (i);
+				}
+			}
+		}
+	}
+	else
+	{
+		return (-1);
+	}
+}
