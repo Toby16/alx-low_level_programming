0c1439529bb3c8b10869ebf42c50382937d46573
Toby16 <bolajibaasit14@gmail.com>
2022-11-25 20:53:49 -0500

    .

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - Function
+ * @h: parameter
+ * Return: elem_num
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t elem_num;
+
+	elem_num = 0;
+
+	while (h != NULL)
+	{
+		elem_num += 1;
+	}
+	return (elem_num);
+}
