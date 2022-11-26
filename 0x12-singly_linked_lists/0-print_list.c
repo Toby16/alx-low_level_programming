28ab73dbb787ad0223b084b3c1249fab5381bb16
Toby16 <bolajibaasit14@gmail.com>
2022-11-25 20:49:11 -0500

    .

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,11 +16,11 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 		{
-			printf("[%d] (%s)\n", 0, "nil");
+			printf("[0] (nil)\n");
 		}
 		else
 		{
-			printf("[%d] (%s)\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		}
 
 		h = h->next;
