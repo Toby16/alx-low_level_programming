de44122aaaeb4b8f70de97c609cff7b8747f8fc5
Toby16 <bolajibaasit14@gmail.com>
2022-12-01 18:15:24 -0500

    .

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * set_bit - Function
+ * @n: parameter
+ * @index: parameter
+ * Return: value
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	*n ^= (1 << index);
+
+	return (1);
+}
