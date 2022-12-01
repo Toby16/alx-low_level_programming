bc5c677bf40b7c31f6500b1de5d8b888754243f7
Toby16 <bolajibaasit14@gmail.com>
2022-11-30 20:28:41 -0500

    .

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint - Function
+ * @head: parameter
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *ptr;
+
+	while (head != NULL)
+	{
+		ptr = head;
+		head = head->next;
+		free(ptr);
+	}
+}
