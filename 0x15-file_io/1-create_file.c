ee1c817f04144032e896465c9bf65cf59dfa19ce
Toby16 <bolajibaasit14@gmail.com>
2022-12-05 21:06:29 -0500

    .

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,10 +27,12 @@ int create_file(const char *filename, char *text_content)
 	}
 
 
-	if (text_content != NULL)
+	if (text_content == NULL)
 	{
-		i = 0;
-
+		text_tontent = "";
+	}
+	else
+	{
 		while (text_content[i])
 		{
 			i += 1;
