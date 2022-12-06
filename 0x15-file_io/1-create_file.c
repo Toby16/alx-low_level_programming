ded837bc6685ab693ce2e385f9ed2ac407f4fd79
Toby16 <bolajibaasit14@gmail.com>
2022-12-05 20:55:28 -0500

    .

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * create_file - function
+ * @filename: parameter
+ * @text_content: parameter
+ * Return: value
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i, write_file;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	else
+	{
+		fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	}
+
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (1);
+	}
+
+	if (text_content != NULL)
+	{
+		i = 0;
+
+		while (text_content[i] != '\0')
+		{
+			i += 1;
+		}
+	}
+
+	write_file = write(fd, text_content, i);
+
+	if (write_file == -1)
+	{
+		return (-1);
+	}
+
+	close(fd);
+
+	return (1);
+}
