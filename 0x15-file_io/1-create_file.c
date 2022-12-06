1002dde352f29554765b0d1ee72793af44fe60a3
Toby16 <bolajibaasit14@gmail.com>
2022-12-05 21:00:46 -0500

    .

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,8 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, i, write_file;
+	int fd, write_file;
+	size_t i;
 
 	if (filename == NULL)
 	{
@@ -17,23 +18,20 @@ int create_file(const char *filename, char *text_content)
 	}
 	else
 	{
-		fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+		fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	}
 
 	if (fd == -1)
 	{
 		return (-1);
 	}
-	else
-	{
-		return (1);
-	}
+
 
 	if (text_content != NULL)
 	{
 		i = 0;
 
-		while (text_content[i] != '\0')
+		while (text_content[i])
 		{
 			i += 1;
 		}
@@ -45,6 +43,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
+
 
 	close(fd);
 
