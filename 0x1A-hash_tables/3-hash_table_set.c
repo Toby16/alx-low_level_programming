622e00d0051c9e9682d743f31116d08b213371c1
Toby16 <bolajibaasit14@gmail.com>
2023-02-03 18:02:03 -0500

    >>> ht['betty'] = 'cool'

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,45 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - function to add element to the hash table
+ * @ht: argument of type 'hash_table_t *'
+ *	hash table
+ * @key: argument og type 'const char *'
+ *	key
+ * @value: argument of type 'const char *'
+ *	value associated wuth key
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned int long i;
+	hash_node_t *temp, *newnode;
+
+	if ((key == NULL) || (value == NULL))
+	{
+		return (0);
+	}
+
+	i = key_index((const unsigned char *)key, ht->size);
+	temp = ht->array[i];
+
+	while (temp != NULL)
+	{
+		if (strcmp(key, temp->key) == 0)
+		{
+			free(temp->value);
+			temp->value = strdup(value);
+			return (1);
+		}
+		temp = temp->next;
+	}
+	newnode = malloc(sizeof(hash_node_t));
+	if (newnode == NULL)
+		return (0);
+	newnode->key = strdup(key);
+	newnode->value = strdup(value);
+
+	newnode->next = ht->array[i];
+	ht->array[i] = newnode;
+	return (1);
+}
