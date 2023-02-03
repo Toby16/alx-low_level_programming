1bc1b98ff498efa33bf53454577f1e61888d21aa
Toby16 <bolajibaasit14@gmail.com>
2023-02-03 18:06:17 -0500

    >>> ht['betty'] = 'cool' | fixed betty error/wraning

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,6 +8,8 @@
  *	key
  * @value: argument of type 'const char *'
  *	value associated wuth key
+ * Return: 1 on success
+ *	else, 0
  */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
