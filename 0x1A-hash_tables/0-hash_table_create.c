6983b7534121f53eed451510f564d4566fece02a
Toby16 <bolajibaasit14@gmail.com>
2023-02-03 11:15:34 -0500

    create a hash table | i hope it passed the checks

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,41 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function to create a hash table
+ * @size: argument of type 'unsigned long int'
+ *	determines the size of the array
+ * Return: hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_table;
+	unsigned long int i;
+
+	new_table = malloc(sizeof(hash_table_t));
+
+	if (new_table == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		new_table->array = malloc(sizeof(void *) * size);
+
+		if (new_table->array == NULL)
+		{
+			return (NULL);
+		}
+		else
+		{
+			i = 0;
+			while (i < size)
+			{
+				new_table->array[i] = NULL;
+				i += 1;
+			}
+		}
+		new_table->size = size;
+	}
+	return (new_table);
+}
