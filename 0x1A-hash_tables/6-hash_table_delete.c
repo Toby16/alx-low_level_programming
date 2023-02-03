84cac0a754452146b834c443e35368cc5ad29639
Toby16 <bolajibaasit14@gmail.com>
2023-02-03 18:41:25 -0500

    >>> del ht | fixed betty linter error/warning

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,5 +1,10 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_delete - function to delete a hash table
+ * @ht: argument of type 'hash_table_t *'
+ */
+
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
