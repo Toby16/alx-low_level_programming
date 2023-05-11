fefbb3acd191ce9c5841accc8ea64ce6f7c9cc9f
Toby16 <bolajibaasit14@gmail.com>
2023-05-11 08:14:54 -0400

    Binary search  | Report: refactored code to suite output requirement

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -32,7 +32,7 @@ int binary_search(int *array, size_t size, int value)
 		{
 			printf("%d, ", array[i]);
 		}
-		printf("%d", array[i]);
+		printf("%d ", array[i]);
 		printf("\n");
 		middle = (left + right) / 2;
 
